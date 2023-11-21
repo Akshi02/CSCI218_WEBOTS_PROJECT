947485b380d3939f3d6914f1d2c01746397f2784
jij927 <josephivan961@gmail.com>
2023-11-21 15:15:11 +0400

    added conditions

--- robot.py
+++ robot.py
@@ -22,128 +22,121 @@ spkr = Sound()
 btn = Button()
 radio = Radio()
 
-#color_sensor_in1 = ColorSensor(INPUT_1)
-#color_sensor_in2 = ColorSensor(INPUT_2)
+# color_sensor_in1 = ColorSensor(INPUT_1)
+# color_sensor_in2 = ColorSensor(INPUT_2)
 ul_sensor_front = UltrasonicSensor(INPUT_2)
 ul_sensor_left = UltrasonicSensor(INPUT_3)
 ul_sensor_right = UltrasonicSensor(INPUT_4)
 gps_sensor = GPSSensor(INPUT_6)
 gyro_sensor = GyroSensor(INPUT_5)
 
-motorC = LargeMotor(OUTPUT_C) # Magnet
+motorC = LargeMotor(OUTPUT_C)  # Magnet
 
 # Here is where your code starts
-ul_sensor_front.MODE_US_DIST_CM = 'US-DIST-CM'
-ul_sensor_left.MODE_US_DIST_CM = 'US-DIST-CM'
-ul_sensor_right.MODE_US_DIST_CM = 'US-DIST-CM'
+ul_sensor_front.MODE_US_DIST_CM = "US-DIST-CM"
+ul_sensor_left.MODE_US_DIST_CM = "US-DIST-CM"
+ul_sensor_right.MODE_US_DIST_CM = "US-DIST-CM"
 
-#face_angle = gyro_sensor.angle
+# face_angle = gyro_sensor.angle
 
-#facing represents which direction the robot is facing
-# 0 represents up 
-# 1 represents right 
-# 2 represents down 
+# facing represents which direction the robot is facing
+# 0 represents up
+# 1 represents right
+# 2 represents down
 # 3 represents left
 facing = 0
 
-#Not to be used. These functions are for making the actions simpler to read.
-def turn_right_90(): 
+
+# Not to be used. These functions are for making the actions simpler to read.
+def turn_right_90():
     global facing
-    
+
     angle = gyro_sensor.angle
     while gyro_sensor.angle < angle + 90:
         tank_drive.on(40, 0)
-    tank_drive.on(0,0)
+    tank_drive.on(0, 0)
     facing += 1
     facing %= 4
-    
-def turn_left_90(): 
+
+
+def turn_left_90():
     global facing
-    
+
     angle = gyro_sensor.angle
     while gyro_sensor.angle > angle - 90:
         tank_drive.on(0, 40)
-    tank_drive.on(0,0)
+    tank_drive.on(0, 0)
     facing -= 1
     facing %= 4
-    
-#Actions
-def move_up(): 
-    global facing
-    
-    #facing right
-    if facing == 1: 
-        turn_left_90()
-    #facing down
-    elif facing == 2: 
-        turn_left_90()
-        turn_left_90()
-    #facing left
-    elif facing == 3: 
-        turn_right_90()
-        
-    tank_drive.on(20,20)
-    
-def move_down(): 
-    global facing
-    
-    #facing up
-    if facing == 0: 
-        turn_left_90()
-        turn_left_90()
-    #facing right
-    elif facing == 1: 
-        turn_right_90()
-    #facing left
-    elif facing == 3: 
-        turn_left_90()
-        
-    tank_drive.on(20,20)
-    
-def move_left(): 
-    #facing up
-    if facing == 0: 
-        turn_left_90()
-    #facing right
-    elif facing == 1: 
-        turn_right_90()
-        turn_right_90()
-    #facing down
-    elif facing == 2: 
-        turn_left_90()
-    
-    tank_drive.on(20,20)
-    
-def move_right(): 
-    #facing up
-    if facing == 0: 
-        turn_right_90()
-    #facing down
-    elif facing == 2: 
-        turn_left_90()
-    #facing left
-    elif facing == 3: 
-        turn_left_90()
-        turn_left_90()
-    tank_drive.on(20,20)
-    
-def facevalue():
+
+
+# Actions
+def move_up():
     global facing
 
-    face_dir = ()
+    # facing right
+    if facing == 1:
+        turn_left_90()
+    # facing down
+    elif facing == 2:
+        turn_left_90()
+        turn_left_90()
+    # facing left
+    elif facing == 3:
+        turn_right_90()
 
-    # facing up & down
-    if facing == 0 or facing == 2: 
-        face_dir = (0, 1, 0)
-    # facing right & left
-    elif facing == 1 or facing == 3: 
-        face_dir = (1, 0, 0)
+    tank_drive.on(20, 20)
 
-    return face_dir
 
-#while True:
+def move_down():
+    global facing
+
+    # facing up
+    if facing == 0:
+        turn_left_90()
+        turn_left_90()
+    # facing right
+    elif facing == 1:
+        turn_right_90()
+    # facing left
+    elif facing == 3:
+        turn_left_90()
+
+    tank_drive.on(20, 20)
+
+
+def move_left():
+    # facing up
+    if facing == 0:
+        turn_left_90()
+    # facing right
+    elif facing == 1:
+        turn_right_90()
+        turn_right_90()
+    # facing down
+    elif facing == 2:
+        turn_left_90()
+
+    tank_drive.on(20, 20)
+
+
+def move_right():
+    # facing up
+    if facing == 0:
+        turn_right_90()
+    # facing down
+    elif facing == 2:
+        turn_left_90()
+    # facing left
+    elif facing == 3:
+        turn_left_90()
+        turn_left_90()
+    tank_drive.on(20, 20)
+
+
+# while True:
 #    if color_sensor_in1.reflected_light_intensity >= 40 and color_sensor_in2.reflected_light_intensity == 0:
-'''
+"""
     if ul_sensor_front.distance_centimeters < 10 and ul_sensor_left.distance_centimeters < ul_sensor_right.distance_centimeters:
         while gyro_sensor.angle < cur_angle + 90:
             tank_drive.on((10), -15)
@@ -160,40 +153,123 @@ def facevalue():
     print(gps_sensor.x)
     print(gps_sensor.y)
     #print("Im in the loop")
-    '''
-    
-#Akshita's States Defination:
+    """
 
+# Akshita's States Defination:
+
+# Define maze dimensions
 maze_width = 15
 maze_height = 15
 
 # Define states
 states = [(x, y) for x in range(maze_width) for y in range(maze_height)]
 
-# Choose a state as the starting point
-start_index = 0  # Change this index as needed
-start = states[start_index]
+# Print the defined states
+print("Defined States:")
+print(states)
 
-# Distance to target
-distance = ul_sensor_front.distance_centimeters
 
-# Direction to target
-direction = facevalue()
+def extract_wall_coordinates(gps_sensor_position, maze_options):
+    if not gps_sensor_position:
+        print("GPSSensor not found in the robot configuration.")
+        return []
 
-# Calculate target coordinates
-magnitude = math.sqrt(sum(d**2 for d in direction))
-target = [round(start[i] + distance * direction[i] / magnitude) for i in range(2)]
+    maze_columns = maze_options.get("columns", 0)
+    maze_rows = maze_options.get("rows", 0)
+    maze_size = maze_options.get("size", 0)
+    wall_thickness = maze_options.get("wallThickness", 0)
 
-# Convert the target coordinates to integers
-target = tuple(map(int, target))
+    robot_position = (int(gps_sensor_position[0]), int(gps_sensor_position[1]))
 
-print(f"Starting point: {start}")
-print(f"Distance to target: {distance}")
-print(f"Direction to target: {direction}")
-print(f"Target coordinates: {target}")
-print(f"GPS Sensor Coordinates: ({gps_sensor.x}, {gps_sensor.y})")
+    walls = []
 
-def get_state(x, y):
-    return (x//40, y//40)
+    for x in range(maze_columns):
+        for y in range(maze_rows):
+            if x == 0 or x == maze_columns - 1 or y == 0 or y == maze_rows - 1:
+                # Considering outer walls as obstacles
+                walls.append((x, y))
+            elif (
+                x % maze_size == 0 and y % maze_size == 0
+            ):  # Assuming walls are placed at regular intervals
+                distance_to_robot = math.sqrt(
+                    (x - robot_position[0]) ** 2 + (y - robot_position[1]) ** 2
+                )
+                walls.append((x, y, distance_to_robot))  # Include distance information
 
-#next step: to use get_state to remove the wall state from the maze state make a function of it.
+    return walls
+
+
+# Example usage
+robot_position = (gps_sensor.X, gps_sensor.Y)  # Replace with actual GPS sensor position
+maze_options = {
+    "columns": 15,
+    "rows": 15,
+    "size": 40,
+    "wallThickness": 2,
+}
+
+wall_coordinates = extract_wall_coordinates(robot_position, maze_options)
+print("Wall Coordinates:")
+print(wall_coordinates)
+
+# ------------------------------------------------------------------------------------------------------------------------------
+# Joseph's code
+
+q_table = [[0, 0, 0, 0], [0, 0, 0, 0]]
+
+gamma = 0.9
+
+reward = -100
+
+
+def computeReward(state, action):
+    if current_pos == goal_pos:
+        return 0
+
+    else:
+        return -1
+
+
+visited = []
+
+
+def compute_Qvalue(state, action, count=0):
+    global visited
+
+    if state not in visited:
+        visited.append(state)
+        reward = 10
+    # define goal state
+    elif state == goal_state:
+        reward = 10000
+    else:
+        reward = -10
+
+    if count == 100:
+        return 0
+
+    q = reward + gamma * max(
+        compute_Qvalue(state[0] + 1, 0, count + 1),
+        max(compute_Qvalue(state[0] - 1, 2, count + 1)),
+        max(compute_Qvalue(state[1] + 1, 1, count + 1)),
+        max(compute_Qvalue(state[1] - 1, 3, count + 1)),
+    )
+
+
+def q_learning(state, action):
+    # q_table = [[0,0,0,0],[0,0,0,0]] list of states and their actions inside them
+
+    # q(state, action) = r(state, action) + gamma * Max[Q_nextState()]
+
+    reward = computeReward()
+
+    Q_next_state = computeNextState(state, action)
+
+    q = compute_Qvalue(state, action)
+    
+    q_table[state][action] = q
+    
+    
+def q_training():
+    
+    
